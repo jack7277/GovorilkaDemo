8ce42d17ca8547584d15700ed998f31d894593ec
Jack7277 <jack7277@gmail.com>
2016-05-13 00:09:38 +0300

    Версия 33 рабочая с сервисом на включение экрана, движок Сфинкс

---
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/lockScreenReceiver.java
@@ -0,0 +1,22 @@
+package edu.cmu.pocketsphinx.demo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class lockScreenReceiver extends BroadcastReceiver {
+
+    public static boolean wasScreenOn = true;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // если экран выключен то запускаем наш лок скрин
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            wasScreenOn = false;
+            Intent intent1 = new Intent(context, GovorilkaActivity.class);
+            intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent1);
+        }
+    }
+}
