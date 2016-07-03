e8692340197f3486750f0c15dd72cbaf80dc059b
Jack7277 <jack7277@gmail.com>
2016-07-04 00:52:57 +0300

    pre 39, 3

--- app/src/main/java/com/govorilka/demo/SettingsActivity.java
+++ app/src/main/java/com/govorilka/demo/SettingsActivity.java
@@ -1,11 +1,16 @@
 package com.govorilka.demo;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by jack on 03.07.2016.
  */
 
 public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+    }
 }
