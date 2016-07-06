d785cd59d6542b5fa61a0d356226a61aa88f5a22
Jack7277 <jack7277@gmail.com>
2016-07-06 23:46:06 +0300

    pre 39, 4

--- app/src/main/java/com/govorilka/demo/SettingsActivity.java
+++ app/src/main/java/com/govorilka/demo/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.govorilka.demo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 /**
  * Created by jack on 03.07.2016.
@@ -12,5 +16,25 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                        | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                        | WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.settings);
     }
+
+    public void callMainScreen(View view) {
+        finish();
+        Intent intent = new Intent(this, GovorilkaActivity.class);
+        startActivity(intent);
+    }
+
+    // отключаем кнопку назад
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(getApplicationContext(), "Только кнопкой 'Назад'", Toast.LENGTH_LONG).show();
+    }
+
+
 }
